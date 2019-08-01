7ee9d1d48def76aa33cdc5c9ffc9ddf4d2e77b84
mmfink <mmfink@users.noreply.github.com>
2019-08-01 09:54:42 -0600

    Some consistency edits
    No hobgoblins here

--- watyrcalcs.py
+++ watyrcalcs.py
@@ -5,7 +5,7 @@ The idea here is to make sure that Winter includes the December from the year *b
 
 @author: Michelle M. Fink, michelle.fink@colostate.edu
          Colorado Natural Heritage Program, Colorado State University
-Created on Oct 24, 2017 Last updated 07/22/2019 - Built on Python 2.7.14
+Created on Oct 24, 2017 Last updated 07/22/2019 - Built on Python 2.7 but works in 3.7
 
 Code licensed under the GNU General Public License version 3.
 This script is free software: you can redistribute it and/or modify
@@ -31,14 +31,13 @@ WATERYR_LU = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:-1, 11:-1, 12:-1}
 ANN_LU = {1:0, 2:0, 12:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0}
 
 def clean_name(inmodel, inseason, invar):
-    """Create a clean naming prefix for output files
-    inputs:
-    inmodel = string; the name of the climate model, usually from nc filename
-      or internal variable value.
-    inseason = integer; taken from SEASONS_LU dictionary
-    invar = string; the nc variable being analyzed
+    """ Create a clean naming prefix for output files
+        inmodel = string; the name of the climate model, or equivalent identifier,
+            usually from nc filename or internal variable value.
+        inseason = integer; taken from SEASONS_LU dictionary
+        invar = string; the nc variable being analyzed
 
-    output: a string to be used as an output file naming prefix
+    returns: a string to be used as an output file naming prefix
     """
     modname = inmodel.replace('.1.', '_')
     modname = modname.replace('-', '_')
@@ -62,16 +61,15 @@ def clean_name(inmodel, inseason, invar):
     return namestr
 
 def watyrmask(orig_dates, datelist, yrlist, season, **kwargs):
-    """Alter the seasonal masks to follow a 'water year' instead of a calendar year
-    inputs:
-    orig_dates = ndarray; the original time values of the netCDF converted to dates
-    datelist = list; the original time values of the netCDF flattened to a list
-    yrlist = list; the range of four-digit years of interest
-    season = integer; taken from SEASONS_LU dictionary
-    kwargs = dictionary; additional variables defining time units and calendar
-      defaults: {'units':'days since 1950-01-01 00:00:00', 'calen':'gregorian'}
+    """ Alter the seasonal masks to follow a 'water year' instead of a calendar year
+        orig_dates = ndarray; the original time values of the netCDF converted to dates
+        datelist = list; the original time values of the netCDF flattened to a list
+        yrlist = list; the range of four-digit years of interest
+        season = integer; taken from SEASONS_LU dictionary
+        kwargs = dictionary; additional variables defining time units and calendar
+            defaults: {'units':'days since 1950-01-01 00:00:00', 'calen':'gregorian'}
 
-    output: a new set of masks
+    returns: a new set of masks
     """
     if kwargs == {}:
         kwargs = {'units':'days since 1950-01-01 00:00:00', 'calen':'gregorian'}
