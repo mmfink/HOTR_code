ee3c8909e0c6fc33186a1c2413f251cd3d3c8f26
mmfink <mmfink@users.noreply.github.com>
2019-09-30 16:25:26 -0600

    Further modeling code refinements

--- HOTR_model_functions.r
+++ HOTR_model_functions.r
@@ -1,6 +1,7 @@
-# Parameter reduction functions
+#### Parameter reduction functions ####
 require(gam)
 require(dplyr)
+require(biomod2)
 
 comb <- function(x, ...) {
   #https://stackoverflow.com/questions/19791609/saving-multiple-outputs-of-foreach-dopar-loop
@@ -18,12 +19,10 @@ devianceExp <- function(dat, y, vars){
     dev.broke <- try((1-g$dev/g$null.deviance)<0, silent = TRUE)
     if(class(dev.broke) == "try-error") {dev.broke = TRUE}
     if("try-error"%in%class(g) | dev.broke){
-      #gam.failed=TRUE
       g <- glm(y~x+x^2, weights=wgt, family = "binomial")
       y.fit <- predict(g, type = "response")
     } else {
       y.fit <- predict.Gam(g, type = "response")
-      #gam.failed=FALSE
     }
     list(i, 100*(1-g$dev/g$null.deviance))
   }
@@ -33,7 +32,7 @@ devianceExp <- function(dat, y, vars){
 }
 
 numCorr <- function(dat, max_cor, dev_exp){
-  # dat has to be just the input vars, nothing else
+  # dat has to be just the numeric input vars, nothing else
   cmat <- cor(dat, use = "pairwise.complete.obs")
   smat <- cor(dat, method = "spearman", use = "pairwise.complete.obs")
 
@@ -65,4 +64,31 @@ numCorr <- function(dat, max_cor, dev_exp){
     }
   }
   return(vartbl)
+}
+
+########
+modpar <- function(m, dat, vars, modopt_obj, otheropts){
+  if(m=="GAM"){  #FIXME: right now can only do random effects with GAM
+    indat <- dat %>% dplyr::select(response, coords.x1, coords.x2, Grid_ID, unlist(vars))
+  } else {
+    indat <- dat %>% dplyr::select(response, coords.x1, coords.x2, unlist(vars))
+  }
+  if(m=="BRT") m <- "GBM"
+  bmdat <- BIOMOD_FormatingData(resp.var = as.data.frame(indat[, 1]),
+                                expl.var = as.data.frame(indat[, 4:ncol(indat)]),
+                                resp.xy = as.data.frame(indat[, 2:3]),
+                                resp.name = "response")
+
+  bmOut <- BIOMOD_Modeling(bmdat, models = m,
+                           models.options = modopt_obj,
+                           NbRunEval = unlist(otheropts[1]),
+                           DataSplit = unlist(otheropts[2]),
+                           models.eval.meth = unlist(otheropts[3]),
+                           SaveObj = TRUE,
+                           modeling.id = paste0(m, "testing"))
+
+  capture.output(get_evaluations(bmOut),
+                 file=file.path(paste0(m,"_evaluation.txt")))
+  capture.output(get_variables_importance(bmOut),
+                 file=file.path(paste0(m,"_variable_importance.txt")))
 }
